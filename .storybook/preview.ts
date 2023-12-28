e6d06ec38af6d655f4760459ab484c06a336ca29
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2023-12-28 13:54:52 +0300

    Add @storybook/addon-themes to .storybook/main.ts and .storybook/preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,7 @@
 import type { Preview } from "@storybook/react";
 import "../src/index.css"; // replace with the name of your tailwind css file
+import { withThemeByClassName } from "@storybook/addon-themes";
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -13,3 +15,13 @@ const preview: Preview = {
 };
 
 export default preview;
+
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      light: "light",
+      dark: "dark",
+    },
+    defaultTheme: "light",
+  }),
+];
