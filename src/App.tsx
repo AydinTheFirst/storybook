5a68ef6f751e8502be9dfb7e029ea502dd3536d5
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2023-12-28 13:53:52 +0300

    Refactor code for improved performance and readability

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import { Button } from "flowbite-react";
+
+export const App = () => {
+  return <Button>Click me</Button>;
+};
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: ["node_modules/flowbite-react/lib/esm/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
